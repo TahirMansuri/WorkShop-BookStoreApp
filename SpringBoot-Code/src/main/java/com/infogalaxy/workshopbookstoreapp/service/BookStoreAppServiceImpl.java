1c41e9d17551f571bd0de6354e3d0fc627517c73
TahirMansuri <tprocks786@gmail.com>
2022-05-16 21:03:01 +0530

    MODIFIED:[Tahir] JMS-OTP sending OTP vai Mail to Customer EMail

--- SpringBoot-Code/src/main/java/com/infogalaxy/workshopbookstoreapp/service/BookStoreAppServiceImpl.java
+++ SpringBoot-Code/src/main/java/com/infogalaxy/workshopbookstoreapp/service/BookStoreAppServiceImpl.java
@@ -3,9 +3,12 @@ package com.infogalaxy.workshopbookstoreapp.service;
 import com.infogalaxy.workshopbookstoreapp.dto.CustomerRegisterDTO;
 import com.infogalaxy.workshopbookstoreapp.entity.CustomerRegisterEntity;
 import com.infogalaxy.workshopbookstoreapp.repository.CustomerRepo;
+import com.infogalaxy.workshopbookstoreapp.util.JMSUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class BookStoreAppServiceImpl implements IBookStoreAppService{
@@ -16,6 +19,9 @@ public class BookStoreAppServiceImpl implements IBookStoreAppService{
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
 
+
+    @Autowired
+    JMSUtil jmsUtil;
     /***
      * Service Implementation for Add Customer to App using BookStoreApp Repository
      * @param customerRegisterDTO
@@ -25,6 +31,8 @@ public class BookStoreAppServiceImpl implements IBookStoreAppService{
     public CustomerRegisterEntity addCustomer(CustomerRegisterDTO customerRegisterDTO) {
         CustomerRegisterEntity customerRegisterEntity = new CustomerRegisterEntity(customerRegisterDTO);
         customerRegisterEntity.setPassword(passwordEncoder.encode(customerRegisterDTO.getPassword()));
+        int random = ThreadLocalRandom.current().nextInt(100000, 1000000);
+        jmsUtil.sendMail(customerRegisterDTO.getEmailId(),"OTP for Addressbook App Login","Hello "+customerRegisterDTO.getFirstName()+", OTP for Your Addressbook App is : "+random);
         return customerRepo.save(customerRegisterEntity);
     }
 }
